e1e3e378a2a4e9646ed98ada7a3deecd9cbad3b9
Andres Vazquez <andres@data99.com.ar>
2020-09-30 10:31:08 -0300

    make sitemap configurable

---
+++ ckanext/datagovcatalog/helpers/sitemap.py
@@ -0,0 +1,11 @@
+from urlparse import urljoin
+from ckan.lib.base import config
+
+
+def get_sitemap_url():
+    s3_url = config.get('ckanext.geodatagov.s3sitemap.aws_s3_url', 'https://filestore.data.gov/')
+    s3_path = config.get('ckanext.geodatagov.s3sitemap.aws_storage_path', 'gsa/catalog-next/sitemap')
+    
+    return urljoin(s3_url, s3_path, 'sitemap.xml')
+    
+
