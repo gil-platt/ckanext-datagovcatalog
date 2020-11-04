2bb18ed0032a793711d61cf160bb399fb1b66d69
Andres Vazquez <andres@data99.com.ar>
2020-11-04 12:38:26 -0300

    Add tracking info

--- ckanext/datagovcatalog/plugin.py
+++ ckanext/datagovcatalog/plugin.py
@@ -1,5 +1,5 @@
 import logging
-
+from ckan.lib.base import config
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 
@@ -13,6 +13,7 @@ class DatagovcatalogPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
     plugins.implements(plugins.IActions)
     plugins.implements(plugins.ITemplateHelpers)
+    plugins.implements(plugins.IPackageController, inherit=True)
 
     # IConfigurer
 
@@ -34,3 +35,16 @@ class DatagovcatalogPlugin(plugins.SingletonPlugin):
         return {
                 'get_sitemap_url': sitemap.get_sitemap_url,
                 }
+    
+    ## IPackageController
+
+    def before_view(self, pkg_dict):
+        
+        if config.get('ckanext.datagovcatalog.add_packages_tracking_info', True):
+            # add tracking information.
+            # CKAN by default hide tracking info for datasets
+            pkg_dict = toolkit.get_action("package_show")({}, {
+                'include_tracking': True,
+                'id': pkg_dict['id']
+            })
+        return pkg_dict
