6f1d793a53a8574478a16f948051cc60b184d624
Julie Kramer <thejuliekramer@gmail.com>
2020-11-17 15:51:06 -0600

    Add the correct path for sitemap

--- ckanext/datagovcatalog/helpers/sitemap.py
+++ ckanext/datagovcatalog/helpers/sitemap.py
@@ -4,7 +4,7 @@ from ckan.lib.base import config
 
 def get_sitemap_url():
     s3_url = config.get('ckanext.geodatagov.s3sitemap.aws_s3_url', 'https://filestore.data.gov/')
-    s3_path = config.get('ckanext.geodatagov.s3sitemap.aws_storage_path', 'gsa/catalog-next/sitemap')
+    s3_path = config.get('ckanext.geodatagov.s3sitemap.aws_storage_path', 'gsa/catalog-next/sitemap/')
     
     return urljoin(s3_url, s3_path, 'sitemap.xml')
     
