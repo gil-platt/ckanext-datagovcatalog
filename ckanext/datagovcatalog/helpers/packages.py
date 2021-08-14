ab5ff81afcb2a0a8ceac3231274e38cc3643dbe7
Nicholas Kumia <nicholas.kumia@gsa.gov>
2021-08-13 23:11:16 -0400

    new: remove accidentally duplicated function

--- ckanext/datagovcatalog/helpers/packages.py
+++ ckanext/datagovcatalog/helpers/packages.py
@@ -8,9 +8,4 @@ def update_tracking_info_to_package(pkg_dict, new_pkg_dict):
             if resource_dict['url'] == new_resource_dict['url']:
                 resource_dict['tracking_summary'] = new_resource_dict['tracking_summary']
 
-    for resource_dict in pkg_dict.get('dataset', []):
-        for new_resource_dict in new_pkg_dict.get('resources', []):
-            if resource_dict['url'] == new_resource_dict['url']:
-                resource_dict['tracking_summary'] = new_resource_dict['tracking_summary']
-
     return pkg_dict
